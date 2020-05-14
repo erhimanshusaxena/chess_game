78c322c664540ce00966eed4d342d6186ba1a1a1
Himanshu Saxena <himanshusaxena@localhost.localdomain>
2020-05-14 19:05:43 +0530

    Added methods for returning coordinates of cell and vice-versa

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,14 +1,25 @@
 require_relative '../src/board'
 
 describe Board do
+    let(:board) { Board.new }
     context 'with initialization' do
-        let(:board) { Board.new }
+        
 
         it "should create a matrix" do
             expect(board.cells.instance_of?(Array)).to eq(true)
         end
         it "should be a size of 8x8" do
-            expect(board.cells.size).to eq(64)
+            expect(board.cells.flatten.size).to eq(64)
+        end
+    end
+    context "when given cell number of the matrix" do
+        it "should return the row and column of the given cell number" do
+            expect(board.get_row_column('E6')).to eql([5,6])
+        end
+    end
+    context "when given coordinates of the matrix" do
+        it "should return cell name of the given coordinates" do
+            expect(board.get_cell_name(6, 6)).to eql("F6")
         end
     end
 end
--- src/board.rb
+++ src/board.rb
@@ -1,12 +1,29 @@
 class Board
     # On object creation create a matrix of 8x8
     def initialize
-        a = ['A','B','C','D','E','F','G','H']
-        b = ['1','2','3','4','5','6','7','8']
-        @cells = a.product(b).map { |x, y| "#{x}#{y}" }
+        n = 8
+        @cells = Array.new(n) { 
+            |i| Array.new(n) { 
+                    |j| "#{(i+65).chr}#{j+1}" 
+                } 
+            }
     end
     # Return cells of the matrix with name
     def cells
         @cells
+    end
+
+    # Return row and column of the cell 
+    def get_row_column(cell)
+        cells.each_with_index do |row, i|
+            if row.include?(cell)
+                return [i + 1, row.index(cell) + 1]
+            end
+        end
+    end
+
+    #return cell name of row and column
+    def get_cell_name(i, j)
+        return cells[i - 1][j - 1]
     end
 end
